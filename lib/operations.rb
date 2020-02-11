8ec26ba71adf3d867b01f0a99f76a8d092b49981
Dakota States <dakota.states@gmail.com>
2020-02-11 04:47:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-if speed
+if speed < 40 || speed > 60
+  return "Not Safe"
+else
+  return "safe"
+end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || > 60 ? "Unsafe" : "Safe"
 end
 	
 
